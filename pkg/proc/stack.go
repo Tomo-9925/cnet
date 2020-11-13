12e0c7b5c07a63533249a50a46b9586f90265377
tomo-9925 <t.okada0325@gmail.com>
2020-11-13 09:53:08 +0000

    Fix print debug log functions of stack

--- pkg/proc/stack.go
+++ pkg/proc/stack.go
@@ -17,22 +17,22 @@ func (s *pidStack) Len() int {
 }
 
 func (s *pidStack) Push(pids ...int) {
-	logrus.WithField("pid_stack", s).Trace("trying to push pid")
+	logrus.WithField("pid_stack", s).Debug("trying to push pid")
 	for i := len(pids) - 1; i >= 0; i-- {
 		s.top = &pidStackElement{pids[i], s.top}
 		s.size++
-		logrus.WithField("pid_stack", s).Trace("pid pushed")
+		logrus.WithField("pid_stack", s).Debug("pid pushed")
 	}
 }
 
 func (s *pidStack) Pop() (pid int) {
-	logrus.WithField("pid_stack", s).Trace("trying to pop pid")
+	logrus.WithField("pid_stack", s).Debug("trying to pop pid")
 	if s.size > 0 {
 		pid, s.top = s.top.pid, s.top.next
 		s.size--
-		logrus.WithField("pid_stack", s).Trace("pid poped")
+		logrus.WithField("pid_stack", s).Debug("pid poped")
 		return
 	}
-	logrus.WithField("pid_stack", s).Trace("pid not poped")
+	logrus.WithField("pid_stack", s).Debug("pid not poped")
 	return -1
 }
